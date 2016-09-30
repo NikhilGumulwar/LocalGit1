f0224fa33960149ba05a4831814ab2366ebb4b83
MOTIFWORKS\Nikhil.gumulwar <nikhil.gumulwar@motifworks.com>
2016-09-30 15:02:19 +0530

    Commit

---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Configuration;
+using EmpDataAccess;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDataEntities entities = new EmployeeDataEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDataEntities entities = new EmployeeDataEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+        [Route("{id:int}/details")]
+        public Employee GetEmployee(int id)
+        {
+            using (EmployeeDataEntities entities = new EmployeeDataEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+    }
+}
---
+++ WebApi/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmpDataAccess;
+
+namespace WebApi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDataEntities entities = new EmployeeDataEntities())
+            {
+                Console.WriteLine("In Student Controller");
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDataEntities entities = new EmployeeDataEntities())
+            {
+                Console.WriteLine("In Student Controller for particular stud");
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
